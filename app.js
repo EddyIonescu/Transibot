5938759dc929d00b5079dd65ae94652d5cf641b1
eddyionescu <eddyionescu@gmail.com>
2017-01-30 01:06:52 -0500

    Modified app.js config reading

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 const 
   bodyParser = require('body-parser'),
-  config = require('prodConfig'),
+  config = require('./config/prodConfig.json'),
   crypto = require('crypto'),
   express = require('express'),
   https = require('https'),  
@@ -27,23 +27,23 @@ app.use(express.static('public'));
 // App Secret can be retrieved from the App Dashboard
 const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
   process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
+  config.appSecret;
 
 // Arbitrary value used to validate a webhook
 const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
   (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('validationToken');
+  config.validationToken;
 
 // Generate a page access token for your page from the App Dashboard
 const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
+  config.pageAccessToken;
 
 // URL where the app is running (include protocol). Used to point to scripts and 
 // assets located at this address. 
 const SERVER_URL = (process.env.SERVER_URL) ?
   (process.env.SERVER_URL) :
-  config.get('serverURL');
+  config.serverURL;
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
   console.error("Missing config values");
